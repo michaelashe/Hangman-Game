7c3b92ac70d09dd09628fdd4616509d8cf525a5c
Cherie Ashe <CherieAshe@Cheries-MacBook-Pro.local>
2017-10-13 13:11:11 -0400

    Files Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,81 @@
+
+
 var playHangman = confirm("Would you like to play HANGMAN?!");
-var wordChoicesArr = ["sword", "shield", "buckler", "spear", "breastplate"];
+var wordChoicesArr = ["sword", "shield", "buckler", "spear", "helm"];
 var wins = 0;
-var guessesRemaining = 12;
+var guessesRemaining = 8;
 var lettersGuessed = [];
 var wordChoices = wordChoicesArr[(Math.floor(Math.random() * wordChoicesArr.length))]; 
 	console.log(wordChoices);
 
 var answerArray = [];
 	 for (var i = 0; i < wordChoices.length; i++) {
-	 	answerArray[i] = "_";
+	 	answerArray.toString();
+	 	answerArray[i] = "__";
+	 	wordChoices.toString();
 
-	 document.querySelector("#word").innerHTML = answerArray.join(" ");
+
+	document.querySelector("#word").innerHTML = answerArray.join(" ");
 		 console.log(answerArray.join(" "));
+
+	document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
 	 }
+
+	 
+
 		
 document.onkeypress = function(event) {
+	var letter = false;
 //This grabs the input from the user and stores it	
 	var	userguess = String.fromCharCode(event.keyCode).toLowerCase();
 		 console.log(userguess);
 //This grabs the guessed attempts and displays them in HTML
 		lettersGuessed += " " + userguess + ",";
-		document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;
-//This substracts the guesses remaining		
-		guessesRemaining--;
-//This displays the number of guesses remaining		
-		document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
+		document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;	
 //If userguess equals a letter in the array then replace the "-" with the correct letter
+	for (var i = 0; i <= answerArray.length; i++) {
+         var bwLetters = answerArray[i];
+                
+       //prevent double typing a letter
+          if (userguess === bwLetters) {  
+          alert("You've already pressed " + userguess);
+          wins = wins -1;
+                 } 
+            }
 
-for (var i = 0; i < wordChoices.length; i++) {
+	for (var i = 0; i < wordChoices.length; i++) {
 	if (userguess === wordChoices[i])	{
 		answerArray[i] = wordChoices[i];
-			document.querySelector("#word").innerHTML = answerArray.join(" ");
+		document.querySelector("#word").innerHTML = answerArray.join(" ");
+		letter = true;	
+
 		}
+	}
+	if (letter === false) {
+		guessesRemaining--;
+		document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
+		console.log("true or false: "+ letter);
+
+	}
+
+	if (letter === true) {
+		wins++;
+		//document.querySelector("#winsCounter").innerHTML = wins;
+		console.log("true or false: "+ letter);
+		console.log("wins " + wins);
+	}
+
+	if (wins === wordChoices.length) {
+		//document.querySelector("#winsCounter").innerHTML = wins;
+		document.querySelector("#WinGame").innerHTML = "YOU WON!!";
+	}
+
+//If user guesses all the letters in the word, the game stops, gives WiN message then restarts
+	if (guessesRemaining === 0) {
+		document.querySelector("#WinGame").innerHTML = "YOU LOST!!!"
+	}
 	
+
 }
-}
+
+
